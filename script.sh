cce5d58ef6c1158c66ea0245d85e3b3cfa2c7652
Antoine <antoine@a-delrue.be>
2023-02-28 10:04:13 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -18,7 +18,7 @@ evaluate_pl() {
   local mapname="$2"
   local region="$3"
 
-  logit "Going to evaluate $1 ($3) against $2"
+  logit "Going to evaluate $1 [$3] against $2"
 
   PROP="/root/output/"$mapname"_proposed.txt"
 
@@ -30,7 +30,7 @@ evaluate_pl() {
   currentVersion=$(aws ec2 describe-managed-prefix-lists --region $3 --filters Name=prefix-list-id,Values=$plid | jq -r '.PrefixLists[].Version')
   CURRENT="/root/output/"$plid"_V"$currentVersion"_Current-CIDRs.txt"
   aws ec2 get-managed-prefix-list-entries --prefix-list-id $plid --region $3 | jq -r '.Entries[].Cidr' | sort > $CURRENT
-  logit "Logged $1 current CIDRs into $CURRENT"
+  logit "Logged $1 [$3] current CIDRs into $CURRENT"
 
   # Finally, evaluate the content of the PL-ID_Vxx_Current-CIDRs.txt against the MAP-NAME_proposed.txt to obtain details on the changes: PL-ID_Vxx_ToBeAdded.txt and PL-ID_Vxx_ToBeRemoved.txt
   # Show what is to be removed
@@ -39,10 +39,10 @@ evaluate_pl() {
 
   # Check if the file is empty
   if [ ! -s "${TBD}" ]; then
-    logit "Nothing to remove from $1"
+    logit "Nothing to remove from $1 [$3]"
     rm $TBD
   else
-    logit "Logged CIDRs to be deleted from $1 into $TBD"
+    logit "Logged CIDRs to be deleted from $1 [$3] into $TBD"
     echo -e "${RED}Will delete those CIDRs:"
     echo -e "$(cat $TBD)${NC}"
   fi
@@ -53,10 +53,10 @@ evaluate_pl() {
 
   # Check if the file is empty
   if [ ! -s "${TBA}" ]; then
-    logit "Nothing to add in $1"
+    logit "Nothing to add in $1 [$3]"
     rm $TBA
   else
-    logit "Logged CIDRs to be added to $1 into $TBA"
+    logit "Logged CIDRs to be added to $1 [$3] into $TBA"
     echo -e "${GREEN}Will add those CIDRs:"
     echo -e "$(cat $TBA)${NC}"
   fi
@@ -64,22 +64,30 @@ evaluate_pl() {
   # Show what doesn't change
   UNC="/root/output/"$plid"_V"$currentVersion"_UnchangedFromCurrent.txt"
   comm -12 $CURRENT $PROP > $UNC
-  logit "Logged unchanged CIDRs in $1 into $UNC"
-  logit "Finished to evaluate $1 ($3) against $2"
+  logit "Logged unchanged CIDRs in $1 [$3] into $UNC"
+  logit "Finished to evaluate $1 [$3] against $2"
 }
 
 refresh_pl() {
   local plid="$1"
   local mapname="$2"
   local region="$3"
-  logit "Going to refresh $1 ($3) with values from $2"
+  logit "Going to refresh $1 [$3] with values from $2"
 
-  evaluate_pl $1 $2
+  evaluate_pl $1 $2 $3
   if [ ! -s "${TBD}" ] && [ ! -s "${TBA}" ]; then
     logit "Nothing to change in $1, exiting now"
     exit 125
   else
-    logit "Changes to be applied on $1 ($3), will continue now"
+    read -p "Changes to be applied on $1 [$3], do you want to continue ? [Y-N]" -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+    	echo
+        echo "Exited by user"
+        exit 130
+    fi
+    logit "Changes to be applied on $1 [$3], will continue now"
   fi
 
   # Removes entries from the specified Prefix List
@@ -88,14 +96,14 @@ refresh_pl() {
 
   # Check if the file is empty
   if [ ! -s "${TBD}" ]; then
-    logit "Nothing to remove from $1 ($3)"
+    logit "Nothing to remove from $1 [$3]"
   else
     for CIDR in $(cat $TBD); do CIDRs_RM="$CIDRs_RM""Cidr=""$CIDR "; done
 
     currentVersion=$(aws ec2 describe-managed-prefix-lists --region $3 --filters Name=prefix-list-id,Values=$plid | jq -r '.PrefixLists[].Version')
-    logit "Current version of $1 ($3) is V$currentVersion"
+    logit "Current version of $1 [$3] is V$currentVersion"
     aws ec2 modify-managed-prefix-list --prefix-list-id $plid --region $3 --remove-entries $CIDRs_RM --current-version $currentVersion >> $LOG_FILE
-    logit "Removed entries from $1 ($3) using content of $TBD"
+    logit "Removed entries from $1 [$3] using content of $TBD"
     echo -e "${RED}Those CIDRs have been removed:"
     echo -e "$CIDRs_RM"${NC}
   fi
@@ -107,10 +115,10 @@ refresh_pl() {
 
   # Check if the file is empty
   if [ ! -s "${TBA}" ]; then
-    logit "Nothing to add in $1 ($3)"
+    logit "Nothing to add in $1 [$3]"
   else
     for CIDR_ADD in $(cat $TBA) ; do CIDRs_ADD="$CIDRs_ADD""Cidr=""$CIDR_ADD "; done
-    logit "Will now add entries to $1 ($3) using content of $TBA"
+    logit "Will now add entries to $1 [$3] using content of $TBA"
 
     currentVersion=$(aws ec2 describe-managed-prefix-lists --region $3 --filters Name=prefix-list-id,Values=$plid | jq -r '.PrefixLists[].Version')
     aws ec2 modify-managed-prefix-list --prefix-list-id $plid --region $3 --add-entries $CIDRs_ADD --current-version $currentVersion >> $LOG_FILE
@@ -119,10 +127,10 @@ refresh_pl() {
 
     # Finally, get the latest version of the Prefix List for documentation and to confirm the changes
     currentVersion=$(aws ec2 describe-managed-prefix-lists --region $3 --filters Name=prefix-list-id,Values=$plid | jq -r '.PrefixLists[].Version')
-    logit "Updated version of $1 ($3) is V$currentVersion"
+    logit "Updated version of $1 [$3] is V$currentVersion"
     UPDT="/root/output/"$plid"_V"$currentVersion"_Current-CIDRs.txt"
     aws ec2 get-managed-prefix-list-entries --prefix-list-id $plid --region $3 | jq -r '.Entries[].Cidr' | sort > $UPDT
-    logit "Logged new version of $1 ($3) into $UPDT"
+    logit "Logged new version of $1 [$3] into $UPDT"
   fi
 }
 
@@ -145,5 +153,5 @@ case "$1" in
   evaluate_pl) "$@"; exit;;
   refresh_pl) "$@"; exit;;  
   empty_pl) "$@"; exit;;      
-  *) log_error "Unkown function: $1()"; exit 2;;
+  *) logit "Unkown function: $1()"; exit 2;;
 esac
